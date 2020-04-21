818b9d8086f06b13252b7fada3399cad5455e102
Parisa <39021781+Parisaaghamohammadi@users.noreply.github.com>
2020-04-22 03:30:02 +0430

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
 
+//show Date
+let d = new getDate()
+
+document.getElementById("demo").innerHTML = d.getDay()
+console.log
+
+//function of to do list
+function additem() {
+    let plad = document.getElementById("addplan").value
+    if (plad == "") {
+        alert("please enter a task")
+    } else {
+        let planLine = document.createElement("p")
+        let planBox = document.createElement("span")
+        let PlanText = document.createTextNode(plad)
+        planBox.appendChild(PlanText)
+        planLine.appendChild(planBox)
+        let result = document.getElementById("result")
+        result.appendChild(planLine)
+        document.getElementById("addplan").value = ""
+        let remTag = document.createElement("span")
+        let remText = document.createTextNode("X")
+        remTag.appendChild(remText)
+        planLine.appendChild(remTag)
+        planLine.addEventListener("click", redLine)
+        function redLine() {
+            planBox.style.textDecoration = "line-through"
+        }
+        let remove = document.createAttribute("onclick")
+        remove.value = "this.parentNode.remove()"
+        remTag.setAttributeNode(remove)
+
+    }
+}
+
+function removeDivFunction() {
+    MyDiv.parentNode.removeChild(MyDiv);
+}
